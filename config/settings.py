a15ffd9f33ac51373c1fc2ca79914aa032e001f2
Biwash <biwashstha864@gmail.com>
2026-07-26 19:24:52 +0545

    Add custom jwt authentication end points

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'common',
     'department',
     'employees',
+    'authentication',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -9,14 +9,16 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("department.urls")),
     path("api/", include("employees.urls")),
+    
     path(
     "api/token/",
     TokenObtainPairView.as_view(),
     name="token_obtain_pair",
 ),
-path(
+    path(
     "api/token/refresh/",
     TokenRefreshView.as_view(),
     name="token_refresh",
 ),
+    path("api/auth/", include("authentication.urls")),
 ]
