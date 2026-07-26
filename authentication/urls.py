8bd695ba86bed1655829d694de3defb20b026db1
Biwash <biwashstha864@gmail.com>
2026-07-26 19:33:52 +0545

    JWT setup without views and serializer

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,16 +1,20 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import LoginView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path(
-        "login/",
-        LoginView.as_view(),
-        name="login",
-    ),
+    "login/",
+    TokenObtainPairView.as_view(),
+    name="token_obtain_pair",
+),
     path(
-        "refresh/",
-        TokenRefreshView.as_view(),
-        name="token_refresh",
-    ),
+    "refresh/",
+    TokenRefreshView.as_view(),
+    name="token_refresh",
+),
 ]
